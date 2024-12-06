772528e75fc9e6168fa7e992261baa5f02041ccb
Jody Clements <jjclements14@gmail.com>
2024-12-06 14:42:33 -0700

    first week of the pet store project.

---
+++ src/main/java/pet/store/entity/Customer.java
@@ -0,0 +1,29 @@
+package pet.store.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class Customer {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long CustomerId;
+  private String CustomerFirstName;
+  private String CustomerLastName;
+  private String CustomerEmail;
+  
+  @EqualsAndHashCode.Exclude
+  @ToString.Exclude
+  @ManyToMany(mappedBy = "customers", cascade = CascadeType.PERSIST)
+  Set<PetStore> petStores = new HashSet<>();
+}
